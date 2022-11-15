db97c7cdc7443a94ad100172267dd6a3a0d31dfb
Dougwash <116963799+Dougwash@users.noreply.github.com>
2022-11-14 19:59:39 -0500

    add Robot & Weapon classes

---
+++ robot.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self, name, health:int, active_weapon) -> None:
+
+        self.name = name
+        self.health = health
+        self.active_weapon = active_weapon
+
+
+        pass
---
+++ weapon.py
@@ -0,0 +1,5 @@
+class Weapon:
+    def __init__(self, name, attact_power:int) -> None:
+        
+        self.name=name
+        self.attact_power = attact_power
